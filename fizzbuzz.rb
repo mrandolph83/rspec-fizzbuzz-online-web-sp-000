253fe332590a88de5ba2128a87d0304dbcd38b30
Matt Smith <mrandolph83@gmail.com>
2020-07-30 03:32:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
+def fizzbuzz
   fizzbuzz(x) = x.to.i
 
 if fizzbuzz(x) % 3 == 0 # if the number int is divisible by 3
