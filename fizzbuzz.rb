54c977a89c782f32862cbd42c9749d5ee8b3d752
Matt Smith <mrandolph83@gmail.com>
2020-07-30 02:50:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,27 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz
+  fizzbuzz(x) = y
+
+if (x.to_i) % 3 == 0 # if the number int is divisible by 3
+
+  y = "Fizz" # Go fizz
+  puts y
+
+elsif (x.to_i) % 5 == 0
+  y =  "Buzz"
+
+  puts y
+
+elsif (x.to_i) % 15 == 0
+
+  y = "FizzBuzz"
+  puts y
+
+else
+  y = "nil"
+  puts y
+
+  end
+end
