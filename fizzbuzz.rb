2aaf5f9056ac9ed838f7370b3b7954b3c96fca0f
Matt Smith <mrandolph83@gmail.com>
2020-07-30 03:25:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,22 +3,22 @@
 
 def fizzbuzz(x)
 
-if (x.to_i) % 3 == 0 # if the number int is divisible by 3
+if x.to_i % 3 == 0 # if the number int is divisible by 3
 
-  return "Fizz" # Go fizz
-
-
-elsif (x.to_i) % 5 == 0
-
-  return "Buzz"
+"Fizz" # Go fizz
 
 
-elsif (x.to_i) % 15 == 0
+elsif x.to_i % 5 == 0
 
-return "FizzBuzz"
+"Buzz"
+
+
+elsif x.to_i % 15 == 0
+
+"FizzBuzz"
 
 else
-return "nil"
+ "nil"
 
 end
 end
