525d84897e4beaba77821fe123b71f3cda2a3b50
Matt Smith <mrandolph83@gmail.com>
2020-07-30 03:51:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,27 +2,19 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
-  x = x.to.i
-
+  
 if x % 3 == 0 # if the number int is divisible by 3
 
-puts "Fizz" # Go fizz
-"Fizz"
-
+"Fizz" # Go fizz
 
 elsif x % 5 == 0
 
-puts "Buzz"
 "Buzz"
 
-
 elsif x % 15 == 0
-
-puts "FizzBuzz"
 "FizzBuzz"
 
 else
- puts "nil"
  "nil"
 
 end
